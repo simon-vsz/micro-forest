a1bdafd83b22fec88509e920b583a9319eeed0d6
simon-vsz <sivelasquezsilva@gmail.com>
2026-07-09 21:49:23 -0500

    feat: add player movement with Rigidbody and new Input System

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MicroForest.Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed = 5f;
+
+        private Rigidbody _rigidbody;
+        private PlayerInput _playerInput;
+        private Vector2 _moveInput;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+        private void Start()
+        {
+            foreach (var actionMap in _playerInput.actions.actionMaps)
+            {
+                if (actionMap.name == "Player")
+                {
+                    actionMap.Enable();
+                }
+                else
+                {
+                    actionMap.Disable();
+                }
+            }
+        }
+
+        public void OnMove(InputValue value)
+        {
+            _moveInput = value.Get<Vector2>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 movement = new Vector3(_moveInput.x, 0f, _moveInput.y);
+            Vector3 newPosition = _rigidbody.position + movement * _moveSpeed * Time.fixedDeltaTime;
+            _rigidbody.MovePosition(newPosition);
+
+            if (movement.sqrMagnitude > 0.01f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(movement);
+                _rigidbody.MoveRotation(targetRotation);
+            }
+        }
+    }
+}
